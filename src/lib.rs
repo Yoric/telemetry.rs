3a4626840ab22743cfc8eb0eaf7a82df4e5485a6
David Rajchenbach-Teller <D.O.Teller@gmail.com>
2015-10-11 21:14:01 +0200

    Issue #17 Looots of doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+//! Telemetry is a mechanism used to capture metrics in an application,
+//! to later store the data locally or upload it to a server for
+//! statistical analysis.
+//!
+//! Examples of usage:
+//!
+//! - capturing the speed of an operation;
+//! - finding out if users are actually using a feature;
+//! - finding out how the duration of a session;
+//! - determine the operating system on which the application is executed;
+//! - determining the configuration of the application;
+//! - capturing the operations that slow down the application;
+//! - determining the amount of I/O performed by the application;
+//! - ...
+//!
+//! To make use of Telemetry, an application needs:
+//!
+//! - an instance of [`telemetry::Service`](services/struct.Service.html), designed
+//!   to hold all the state in a dedicated thread and export it as needed;
+//! - one or more instances of
+//!   [`telemetry::Feature`](services/struct.Feature.html), designed allow activating
+//!   or deactivating a group of histograms together;
+//! - one or more instances of either
+//!   [`single::Histogram`](single/trait.Histogram.html) or
+//!   [`keyed::KeyedHistogram`](keyed/trait.KeyedHistogram.html), designed to
+//!   actually record the data.
+//!
+//!
+//! Memory note: the memory used by a histogram is recollected only
+//! when its instance of `telemetry` is garbage-collected. In other
+//! words, if a histogram goes out of scope for some reason, its data
+//! remains in telemetry and will be stored and/or uploaded in
+//! accordance with the configuration of this telemetry instance.
+
+
 extern crate rustc_serialize;
 
 
@@ -12,38 +47,11 @@ pub mod single;
 pub mod keyed;
 mod service;
 pub use service::{Feature, Service};
-
-
-// Telemetry is a mechanism used to capture metrics in an application,
-// to later store the data locally or upload it to a server for
-// statistical analysis.
-//
-// Examples of usage:
-// - capturing the speed of an operation;
-// - finding out if users are actually using a feature;
-// - finding out how the duration of a session;
-// - determine the operating system on which the application is executed;
-// - determining the configuration of the application;
-// - capturing the operations that slow down the application;
-// - determining the amount of I/O performed by the application;
-// - ...
-//
-// The abstraction used by this library is the Histogram. Each
-// Histogram serves to capture a specific measurement, store it
-// locally and/or upload it to the server. Several types of Histograms
-// are provided, suited to distinct kinds of measures.
-//
-//
-// Memory note: the memory used by a histogram is recollected only
-// when its instance of `telemetry` is garbage-collected. In other words,
-// if a histogram goes out of scope for some reason, its data remains
-// in telemetry and will be stored and/or uploaded in accordance with the
-// configuration of this telemetry instance.
-//
+pub use keyed::KeyedHistogram;
+pub use single::Histogram;
 
 
 
-//////////////////////////////////// Tests
 
 #[cfg(test)]
 mod tests {
@@ -56,18 +64,17 @@ mod tests {
 
     use super::*;
 
-
     #[test]
     fn create_flags() {
         let telemetry = Arc::new(Service::new([0, 0, 0, 0]));
         let feature = Feature::new(&telemetry);
-        let flag_single = SingleFlag::new(&feature, Metadata { key: "Test linear single".to_string(), expires: None});
-        let flag_map = KeyedFlag::new(&feature, Metadata { key: "Test flag map".to_string(), expires: None});
+        let flag_single = single::Flag::new(&feature, Metadata { key: "Test linear single".to_string(), expires: None});
+        let flag_map = keyed::KeyedFlag::new(&feature, Metadata { key: "Test flag map".to_string(), expires: None});
 
         flag_single.record(());
         flag_map.record("key".to_string(), ());
 
-        feature.is_active.set(true);
+        feature.set_active(true);
         flag_single.record(());
         flag_map.record("key".to_string(), ());
     }
@@ -77,13 +84,13 @@ mod tests {
         let telemetry = Arc::new(Service::new([0, 0, 0, 0]));
         let feature = Feature::new(&telemetry);
         let linear_single =
-            SingleLinear::new(&feature,
+            single::Linear::new(&feature,
                               Metadata {
                                   key: "Test linear single".to_string(),
                                   expires: None
                               }, 0, 100, 10);
         let linear_map =
-            KeyedLinear::new(&feature,
+            keyed::KeyedLinear::new(&feature,
                               Metadata {
                                   key: "Test linear map".to_string(),
                                   expires: None
@@ -92,7 +99,7 @@ mod tests {
         linear_single.record(0);
         linear_map.record("key".to_string(), 0);
 
-        feature.is_active.set(true);
+        feature.set_active(true);
         linear_single.record(0);
         linear_map.record("key".to_string(), 0);
     }
@@ -102,20 +109,21 @@ mod tests {
         let telemetry = Arc::new(Service::new([0, 0, 0, 0]));
         let feature = Feature::new(&telemetry);
 
-        feature.is_active.set(true);
+        feature.set_active(true);
 
         // A single flag that will remain untouched.
         let flag_single_1_name = "Test linear single 1".to_string();
-        let flag_single_1 = SingleFlag::new(&feature, Metadata { key: flag_single_1_name.clone(), expires: None});
+        let flag_single_1 = single::Flag::new(&feature, Metadata { key: flag_single_1_name.clone(), expires: None});
+        let _ = flag_single_1; // Silence an unused variable warning.
 
         // A single flag that will be recorded once.
         let flag_single_2_name = "Test linear single 2".to_string();
-        let flag_single_2 = SingleFlag::new(&feature, Metadata { key: flag_single_2_name.clone(), expires: None});
+        let flag_single_2 = single::Flag::new(&feature, Metadata { key: flag_single_2_name.clone(), expires: None});
         flag_single_2.record(());
 
         // A map flag.
         let flag_map_name = "Test flag map".to_string();
-        let flag_map = KeyedFlag::new(&feature, Metadata { key: flag_map_name.clone(), expires: None});
+        let flag_map = keyed::KeyedFlag::new(&feature, Metadata { key: flag_map_name.clone(), expires: None});
         let key1 = "key 1".to_string();
         let key2 = "key 2".to_string();
         flag_map.record(key1.clone(), ());
--- src/misc.rs
+++ src/misc.rs
@@ -3,29 +3,45 @@ pub struct NamedStorage<T: ?Sized> {
     pub contents: Box<T>,
 }
 
-
+///
+/// A subformat of Json to use for serialization.
+///
 pub enum SerializationFormat {
-    Simple,
+    ///
+    /// Simple Json:
+    /// - `Flag` are represented as a single boolean;
+    /// - `KeyedFlag` are represented as an array;
+    /// - `Linear` are represented as an array of numbers, one cell per bucket;
+    /// - `KeyedLinear` are represented as an object, one field per histogram,
+    ///    with name = key, value = array of numbers as for `Linear`;
+    /// - ...
+    ///
+    SimpleJson,
 }
 
-//
-// A software version, e.g. [2015, 10, 10, 0]
-//
+///
+/// A software version, e.g. [2015, 10, 10, 0].
+///
 pub type Version = [u32;4];
 
-//
-// Metadata on a histogram.
-//
+///
+/// Metadata on a histogram.
+///
 pub struct Metadata {
-    // A key used to identify the histogram. Must be unique to the instance
-    // of `telemetry`.
+    /// A key used to identify the histogram. Must be unique to the
+    /// instance of `telemetry`.
     pub key: String,
 
-    // Optionally, a version of the product at which this histogram expires.
+    /// Optionally, a version of the product at which this histogram
+    /// expires. If the version of the current product is at least as
+    /// recent as `expires`, nothing will be recorded for the
+    /// histogram, and no memory will be used.
     pub expires: Option<Version>,
 }
 
-
+///
+/// A value that can be represented as a u32.
+///
 pub trait Flatten {
     fn as_u32(&self) -> u32;
 }
@@ -36,7 +52,9 @@ impl Flatten for u32 {
     }
 }
 
-
+//
+// Representation of buckets shared by both single and keyed linear histograms.
+//
 pub struct LinearBuckets {
     pub min: u32,
     pub max: u32, // Invariant: max > min
@@ -56,4 +74,18 @@ impl LinearBuckets {
             res as usize
         }
     }
+}
+
+
+pub fn vec_with_size<T>(size: usize, value: T) -> Vec<T> where T: Clone {
+    let mut vec = Vec::with_capacity(size);
+    unsafe {
+        // Resize. In future versions of Rust, we should
+        // be able to use `vec.resize`.
+        vec.set_len(size);
+        for i in 0 .. size - 1 {
+            vec[i] = value.clone();
+        }
+    }
+    vec
 }
