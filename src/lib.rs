2191bd65e9ce7deeb1722300932b4954bb765bf0
David Rajchenbach-Teller <D.O.Teller@gmail.com>
2015-10-12 14:54:24 +0200

    Preparing for #20 - releasing on crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 //! Examples of usage:
 //!
 //! - capturing the speed of an operation;
+//! - finding out if a remote service is often down, and how much impact this has on users;
 //! - finding out if users are actually using a feature;
 //! - finding out how the duration of a session;
 //! - determine the operating system on which the application is executed;
@@ -15,17 +16,9 @@
 //! - ...
 //!
 //!
-//! To make use of Telemetry, an application needs:
+//! This crate provides an API for recording such data in _Histograms_
+//! and then serializing the data
 //!
-//! - an instance of [`telemetry::Service`](services/struct.Service.html), designed
-//!   to hold all the state in a dedicated thread and export it as needed;
-//! - one or more instances of
-//!   [`telemetry::Feature`](services/struct.Feature.html), designed allow activating
-//!   or deactivating a group of histograms together;
-//! - one or more instances of either
-//!   [`single::Histogram`](single/trait.Histogram.html) or
-//!   [`keyed::KeyedHistogram`](keyed/trait.KeyedHistogram.html), designed to
-//!   actually record the data.
 //!
 //!
 //! Memory note: the memory used by a histogram is recollected only
